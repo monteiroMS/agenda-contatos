8240e265815816417b337a7f289a08a0d2405efe
Matheus Monteiro <matheuschran@hotmail.com>
2025-02-13 23:07:46 -0300

    feat: layout base do app

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+import Layout from '../../Layout/App.vue'
+import { Button } from '@/components/ui/button/index'
+
+</script>
+
+<template>
+    <Layout>
+        <Head title="Meus contatos | Agenda de contatos" />
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">
+            Meus contatos
+          </h1>
+        </div>
+        <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+            <div class="flex flex-col items-center gap-1 text-center">
+                <h3 class="text-2xl font-bold tracking-tight">
+                    Você ainda não possui nenhum contato cadastrado :c
+                </h3>
+                <p class="text-sm text-muted-foreground">
+                    Você pode adicionar novos contatos a qualquer momento
+                </p>
+                <Button class="mt-4">
+                    Novo contato
+                </Button>
+            </div>
+        </div>
+    </Layout>
+</template>
