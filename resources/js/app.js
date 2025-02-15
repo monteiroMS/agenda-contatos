0eb63bbfe0bdc1cb6cc39d34e915a01c3744a692
Matheus Monteiro <matheuschran@hotmail.com>
2025-02-15 19:50:33 -0300

    feat: instala diretiva de máscaras para o vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import '../css/app.css';
 
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import VueTheMask from "vue-the-mask";
 
 createInertiaApp({
   resolve: name => {
@@ -12,6 +13,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(VueTheMask)
       .mount(el)
   },
 })
